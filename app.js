224e4e27c0f028b7a49a242c645f0cb7d4ce1d90
Real Ursus <real.ursus@outlook.com>
2025-11-15 10:25:58 -0500

    Disable the strictTransportSecurity middleware

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ require('./jobs/dailyTasks');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(helmet());
+// Disable the strictTransportSecurity middleware to avoid conflicts with HTTPS setups
+app.use(helmet({ strictTransportSecurity: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
